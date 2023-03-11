98130ca304b614ebf0ed2479727695b2d2f4dab5
Babb4ge <atwoodaiden1@gmail.com>
2023-03-11 14:01:01 -0600

    Logged Info

---
+++ FileInformation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Net.Http.Headers;
+
+namespace FileDownloader
+{
+    public class FileInformation
+    {
+
+        static async Task<long> GetFileSizeAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url)))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        if (response.Content.Headers.TryGetValues("Content-Length", out var values))
+                        {
+
+                            long filesize = long.Parse(values.First());
+                            return filesize;
+                        }
+                        else
+                        {
+                            throw new WebException("Unable to parse the content length");
+                        }
+                    }
+                    else
+                    {
+                        throw new WebException($"Error retreiving the file size: {response.StatusCode}");
+                    }
+                }
+            }
+        }
+        public static long FiSize(string url)
+        {
+            return GetFileSizeAsync(url).Result;
+
+        }
+        //Retreives the file type
+        static async Task<string> GetFileTypeAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url)))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        if (response.Content.Headers.TryGetValues("Content-Type", out var values))
+                        {
+                            var mediaType = MediaTypeHeaderValue.Parse(values.First());
+                            return mediaType.MediaType.ToString();
+                        }
+                        else
+                        {
+                            return "Cannot retrieve content type";
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        return $"Error retrieving the download link: {response.StatusCode}";
+                    }
+                }
+            }
+        }
+
+        public static string FileType(string url)
+        {
+            return GetFileTypeAsync(url).Result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Threading.Tasks;
+
+
 
 namespace FileDownloader
 {
@@ -12,65 +18,51 @@ namespace FileDownloader
         {
             await FFDLMAIN();
         }
-
         static async Task FFDLMAIN()
         {
-            string Link;
-            string PostFilePath = @"C: \Users\Derpy\Downloads";
-            do
-            {
-                Console.WriteLine("Paste the Download Link here");
-                Console.Write(">");
-                Link = Console.ReadLine();
 
-                if (string.IsNullOrEmpty(Link) || !Uri.IsWellFormedUriString(Link, UriKind.Absolute))
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid Link Try Again | Press Anything to Continue");
-                    Console.ReadKey(true);
-                    Console.Clear();
-                }
-            } while ((string.IsNullOrEmpty(Link) || !Uri.IsWellFormedUriString(Link, UriKind.Absolute)));
+            Console.WriteLine("Paste the Download Link here");
+            Console.Write(">");
+            string Link = Console.ReadLine()??"";
 
-            using HttpClient client = new HttpClient();
-            using HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, Link));
-            
+            string PostFilePath = @"C: \Users\Derpy\Downloads\";
+            //Pulls Download Prompt
+            //Gets File Size
+            long filesize = FileInformation.FiSize(Link);
+            string math = IMath.ConvertViaMath(filesize);
+            Console.WriteLine(math + " " + Link);
+            Console.WriteLine("What would you like to name the file?");
+            string fileName = Console.ReadLine()??"";
+            Console.Clear();
 
-            if (response.IsSuccessStatusCode)
-            {
-                Console.Clear();
-            }
-
-
-            var contentDisposition = response.Content.Headers.ContentDisposition;
-            
-            string contenttype = response.Content.Headers.ContentType.MediaType;
-            string viewtype = contenttype;
-            string[] parts = contenttype.Split("/");
+            string filetype = FileInformation.FileType(Link);
+            string[] parts = filetype.Split("/");
             string Fcontenttype = ("." + parts[1]);
 
-            Console.WriteLine("Is this the correct file type? || " + Fcontenttype + "\n Press 1 for YES\n Press 2 for NO");
+            Console.WriteLine("Is this the correct file type? || " + fileName + Fcontenttype + "\n Press 1 for YES\n Press 2 for NO");
+
             ConsoleKey key;
             do
             {
+
                 key = Console.ReadKey(true).Key;
                 switch (key)
                 {
                     case ConsoleKey.D1:
                         Console.Clear();
-                        Finalize();
+                        SelfFinalize(fileName, Fcontenttype, Link);
                         break;
                     case ConsoleKey.NumPad1:
                         Console.Clear();
-                        Finalize();
+                        SelfFinalize(fileName, Fcontenttype, Link);
                         break;
                     case ConsoleKey.D2:
                         Console.Clear();
-                        SelfType(contentDisposition.FileName);
+                        SelfType(fileName, Link);
                         break;
                     case ConsoleKey.NumPad2:
                         Console.Clear();
-                        SelfType(contentDisposition.FileName);
+                        SelfType(fileName, Link);
                         break;
                 }
             }
@@ -79,24 +71,58 @@ namespace FileDownloader
 
 
 
-        static void SelfType(string FileName)
+        private static void SelfType(string fileName, string Link)
         {
-
+            Console.WriteLine(fileName + Link);
             Console.WriteLine("Input the correct file type");
             Console.Write(">");
             string NewPreType = Console.ReadLine();
-            string NewPostType = "." + NewPreType;
-            Console.WriteLine(FileName + NewPostType);
-        }
+            string Fcontenttype = "." + NewPreType;
+            Console.Clear();
+            SelfFinalize(fileName, Fcontenttype, Link);
 
-        static void Finalize()
+
+        }
+        //broken not wokring
+        static async Task SelfFinalize(string fileName, string Fcontenttype, string Link)
         {
+            Console.WriteLine(fileName + Fcontenttype, Link);
+            long filesize;
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(Link, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
+                filesize = FileInformation.FiSize(Link);
+            }
 
+            int chunksize = (int)Math.Ceiling(Convert.ToDouble(filesize) / 4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                int start = i * chunksize;
+                int end = (i == 3) ? (int)filesize - 1 : start + chunksize - 1;
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Range = new RangeHeaderValue(start, end);
+                    using (var response = await client.GetAsync(Link, HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            using (var fileStream = new FileStream("C:\\Users\\Derpy\\Downloads\\" + fileName + "." + Fcontenttype, FileMode.Create, FileAccess.Write, FileShare.None))
+                            {
+                                await stream.CopyToAsync(fileStream);
+                            }
+                        }
+                    }
+                }
+            }
         }
-
     }
+}
 
-    }
+
 
 
     
